aa114edb8e840bf92f3d958de0b0ffca98419a8c
Filip3ra <filipijardimrm@gmail.com>
2026-02-02 09:23:41 -0300

    -feat: rotas get e post funcionando

--- backend/src/routes/simulation.routes.js
+++ backend/src/routes/simulation.routes.js
@@ -1,2 +1,13 @@
-import { Route } from "express";
+import { Router } from "express";
 
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ status: "rota /simulate funcionando" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "Simulação recebida com sucesso" });
+});
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import simulationRoutes from "./routes/simulation.routes.js"; // import routes from file 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/simulate", simulationRoutes); // exec message from imported file 
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
